3f99edd8e18bb494902bf1c96f2bbfebc793a96c
ZhangYi <zhangyi@goldensecure.com>
2019-03-31 23:24:05 +0800

    change lib api

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ use tokio_core::reactor::Core;
 
 use rtnetlink::new_connection;
 use rtnetlink::packet::{AddressNla, LinkNla};
+use rtnetlink::Handle;
 
 #[derive(Debug)]
 pub struct IpSettingError {
@@ -27,128 +28,128 @@ impl Error for IpSettingError {
     }
 }
 
-pub fn make_ip_addr_operaters() -> (
-    impl Fn(String) -> Result<Vec<(Ipv4Addr, u8)>, Box<dyn Error>>,
-    impl Fn(String, Ipv4Addr, Ipv4Addr, u8) -> Result<(), IpSettingError>,
-) {
+pub fn make_handle() -> Handle {
     let (connection, handle) = new_connection().unwrap();
-    let handle1 = handle.clone();
-
     spawn(move || Core::new().unwrap().run(connection));
+    handle
+}
 
-    let getter = move |ifname| {
-        // get all address messages
-        let addrs = handle1
-            .address()
-            .get()
-            .execute()
-            .collect()
-            .wait()
-            .expect("get ip address failed");
+pub fn get_ip_addrs(handle: Handle, ifname: String) -> Result<Vec<(Ipv4Addr, u8)>, ()> {
+    // get all address messages
+    let addrs = handle
+        .address()
+        .get()
+        .execute()
+        .collect()
+        .wait()
+        .expect("get ip address failed");
 
-        // get address messages which's label equal to ifname
-        let addrs_iter = addrs.into_iter().filter(|a| {
-            a.nlas.iter().fold(false, |acc, nla| {
-                acc || match nla {
-                    AddressNla::Label(s) => *s == ifname,
-                    _ => false,
-                }
-            })
-        });
+    // get address messages which's label equal to ifname
+    let addrs_iter = addrs.into_iter().filter(|a| {
+        a.nlas.iter().fold(false, |acc, nla| {
+            acc || match nla {
+                AddressNla::Label(s) => *s == ifname,
+                _ => false,
+            }
+        })
+    });
 
-        let mut addrs = vec![];
+    let mut addrs = vec![];
 
-        for addr_msg in addrs_iter {
-            for nla in addr_msg.nlas {
-                match nla {
-                    AddressNla::Address(addr) => addrs.push((
-                        Ipv4Addr::new(addr[0], addr[1], addr[2], addr[3]),
-                        addr_msg.header.prefix_len,
-                    )),
-                    _ => (),
+    for addr_msg in addrs_iter {
+        for nla in addr_msg.nlas {
+            match nla {
+                AddressNla::Address(addr) => addrs.push((
+                    Ipv4Addr::new(addr[0], addr[1], addr[2], addr[3]),
+                    addr_msg.header.prefix_len,
+                )),
+                _ => (),
+            }
+        }
+    }
+
+    Ok(addrs)
+}
+
+pub fn set_ip_addr(
+    handle: Handle,
+    ifname: String,
+    reserved_addr: Ipv4Addr,
+    new_addr: Ipv4Addr,
+    prefix_len: u8,
+) -> Result<(), IpSettingError> {
+    // get all address messages
+    let addrs = handle
+        .address()
+        .get()
+        .execute()
+        .collect()
+        .wait()
+        .expect("get ip address failed");
+
+    // get address messages which's label equal to ifname
+    let addrs_iter = addrs.into_iter().filter(|a| {
+        a.nlas.iter().fold(false, |acc, nla| {
+            acc || match nla {
+                AddressNla::Label(s) => *s == ifname,
+                _ => false,
+            }
+        })
+    });
+
+    let mut index: Option<u32> = None;
+
+    // del all ip address which is not reserved address
+    for addr in addrs_iter {
+        index = Some(addr.header.index);
+        for nla in addr.nlas {
+            if let AddressNla::Address(a) = nla {
+                if a != reserved_addr.octets() {
+                    handle
+                        .address()
+                        .del(
+                            addr.header.index,
+                            IpAddr::V4(Ipv4Addr::new(a[0], a[1], a[2], a[3])),
+                            addr.header.prefix_len,
+                        )
+                        .execute()
+                        .wait()
+                        .expect("del ip address failed");
                 }
             }
         }
+    }
 
-        Ok(addrs)
-    };
-
-    let setter = move |ifname, reserved_addr: Ipv4Addr, new_addr, prefix_len| {
-        // get all address messages
-        let addrs = handle
-            .address()
-            .get()
-            .execute()
-            .collect()
-            .wait()
-            .expect("get ip address failed");
-
-        // get address messages which's label equal to ifname
-        let addrs_iter = addrs.into_iter().filter(|a| {
-            a.nlas.iter().fold(false, |acc, nla| {
-                acc || match nla {
-                    AddressNla::Label(s) => *s == ifname,
-                    _ => false,
-                }
-            })
-        });
-
-        let mut index: Option<u32> = None;
-
-        // del all ip address which is not reserved address
-        for addr in addrs_iter {
-            index = Some(addr.header.index);
-            for nla in addr.nlas {
-                if let AddressNla::Address(a) = nla {
-                    if a != reserved_addr.octets() {
-                        handle
-                            .address()
-                            .del(
-                                addr.header.index,
-                                IpAddr::V4(Ipv4Addr::new(a[0], a[1], a[2], a[3])),
-                                addr.header.prefix_len,
-                            )
-                            .execute()
-                            .wait()
-                            .expect("del ip address failed");
-                    }
-                }
-            }
+    // add new ip address
+    match index {
+        Some(i) => {
+            handle
+                .address()
+                .add(i, IpAddr::V4(new_addr), prefix_len)
+                .execute()
+                .wait()
+                .expect("add ip address failed");
+            Ok(())
         }
-
-        // add new ip address
-        match index {
-            Some(i) => {
-                handle
-                    .address()
-                    .add(i, IpAddr::V4(new_addr), prefix_len)
-                    .execute()
-                    .wait()
-                    .expect("add ip address failed");
-                Ok(())
-            }
-            // the ifname is not exist or have no ip address
-            None => {
-                let links = handle.link().get().execute().collect().wait().unwrap();
-                for link in links {
-                    for nla in link.nlas() {
-                        if let LinkNla::IfName(s) = nla {
-                            if *s == ifname {
-                                handle
-                                    .address()
-                                    .add(link.header().index(), IpAddr::V4(new_addr), prefix_len)
-                                    .execute()
-                                    .wait()
-                                    .expect("add ip address failed");
-                                return Ok(());
-                            }
+        // the ifname is not exist or have no ip address
+        None => {
+            let links = handle.link().get().execute().collect().wait().unwrap();
+            for link in links {
+                for nla in link.nlas() {
+                    if let LinkNla::IfName(s) = nla {
+                        if *s == ifname {
+                            handle
+                                .address()
+                                .add(link.header().index(), IpAddr::V4(new_addr), prefix_len)
+                                .execute()
+                                .wait()
+                                .expect("add ip address failed");
+                            return Ok(());
                         }
                     }
                 }
-                Err(IpSettingError { dev: ifname })
             }
+            Err(IpSettingError { dev: ifname })
         }
-    };
-
-    (getter, setter)
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use ip_addr_op;
+use ip_addr_op::*;
 use std::env;
 use std::net::Ipv4Addr;
 
@@ -10,8 +10,9 @@ fn main() {
     let new_addr = args[3].parse::<Ipv4Addr>().unwrap();
     let prefix_len = args[4].parse::<u8>().unwrap();
 
-    let (get, set) = ip_addr_op::make_ip_addr_operaters();
-    match get(ifname.clone()) {
+    let handle = make_handle();
+
+    match get_ip_addrs(handle.clone(), ifname.clone()) {
         Ok(addrs) => {
             for addr in addrs {
                 println!("{:?}", addr)
@@ -20,7 +21,7 @@ fn main() {
         Err(e) => eprintln!("{:?}", e),
     }
 
-    match set(ifname, reserved_addr, new_addr, prefix_len) {
+    match set_ip_addr(handle.clone(), ifname, reserved_addr, new_addr, prefix_len) {
         Ok(_) => println!("set ip address succeed"),
         Err(e) => eprintln!("{:?}", e),
     };
